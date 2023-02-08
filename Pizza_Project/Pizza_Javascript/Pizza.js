8d3c6055dda8d5273f67a877220e1801a2a4f179
LoafyLilu <liluloaf@gmail.com>
2023-02-08 12:43:11 -0800

    assignment update

--- Pizza_Project/Pizza_Javascript/Pizza.js
+++ Pizza_Project/Pizza_Javascript/Pizza.js
@@ -14,6 +14,8 @@ function getReceipt() {
         }
     }
 
+
+    //Starts with the base price of the selected size
     if (selectedSize === "Personal Pizza") {
         sizeTotal = 6;
     } else if (selectedSize === "Small Pizza") {
@@ -25,6 +27,7 @@ function getReceipt() {
     } else if (selectedSize === "Extra Large Pizza") {
         sizeTotal = 16;
     }
+    //holds the value as running total, to then be used in another funtion
     runningTotal = sizeTotal;
     console.log(selectedSize + " = $" + sizeTotal + ".00");
     console.log("size text1: " + text1);
@@ -32,7 +35,8 @@ function getReceipt() {
     //these variables will get passed on to each function
     getTopping(runningTotal, text1);
 }
-
+//starts the total toppings at 0, and adds each element in the topping array if it is checked. The value is then pushed to 
+//the consol to show what toppings were selected
     function getTopping(runningTotal, text1) {
         var toppingTotal = 0;
         var selectedTopping = [];
@@ -44,14 +48,14 @@ function getReceipt() {
                 text1 = text1 + toppingArray[j].value + "<br>";
             }
         }
-
+// accounting for the 1 free topping
         var toppingCount = selectedTopping.length;
         if (toppingCount > 1) {
             toppingTotal = (toppingCount - 1);
         } else {
             toppingTotal = 0;
         }
-
+//tells the document to display the string value of selected topppings, and the totalPrice, from runningTotal+ (toppingTotal -1)
         runningTotal = (runningTotal + toppingTotal);
         console.log("total selected topping items:" + toppingCount);
         console.log(toppingCount + " topping - 1 free topping = " + "$" + toppingTotal + ".00");
